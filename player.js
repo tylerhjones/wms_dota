fb3096a3e77e2dc44ccd4d3b0abbb7cb1402a075
tylerjones <tyhampjone@gmail.com>
2013-11-21 00:02:46 -0700

    testing

--- player.js
+++ player.js
@@ -1,7 +1,8 @@
 
 function Init()
 {
+	alert(playlist.columns);
 	playlist.setColumnResizeMode( 2, "Fixed" );
-	playlist.setColumnWidth(2,"100")
+	// playlist.setColumnWidth(2,"100")
 	// playlist.setColumnResizeMode( 1, "AutoSizeData" );
 }
