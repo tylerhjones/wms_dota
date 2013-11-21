dc624ebc7666325fadd3f5c9fe2852c544ae63fe
tylerjones <tyhampjone@gmail.com>
2013-11-21 00:04:40 -0700

    fix

--- player.js
+++ player.js
@@ -1,7 +1,7 @@
 
 function Init()
 {
-	playlist.setColumnResizeMode( 2, "Fixed" );
+	// playlist.setColumnResizeMode( 2, "Fixed" );
 	playlist.setColumnWidth(2,"400")
 	// playlist.setColumnResizeMode( 1, "AutoSizeData" );
 }
