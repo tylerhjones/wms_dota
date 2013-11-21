917254e41c26d6ea546c05d06ab73633b31affa0
tylerjones <tyhampjone@gmail.com>
2013-11-21 00:09:38 -0700

    tstng

--- player.js
+++ player.js
@@ -2,6 +2,6 @@
 function Init()
 {
 	playlist.setColumnResizeMode( 0, "Stretches" );
-	playlist.setColumnResizeMode( 1, "Stretches" );
+	// playlist.setColumnResizeMode( 1, "Stretches" );
 	// playlist.setColumnResizeMode( 2, "Stretches" );
 }
